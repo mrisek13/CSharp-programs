e80e4c520b0f0f0206bd8996192aefd4d87f88b2
Maja Risek <108302992+mrisek13@users.noreply.github.com>
2023-10-21 22:20:44 +0200

    Add files via upload

---
+++ class/Pravokutnik.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Vj3
+{
+    //1.	Ispravno definiran naziv razreda
+    class Pravokutnik
+    {
+        //2.    Podatak o dužini stranice A tipa float dostupan samo unutar razreda
+        float stranicaA;
+
+        //3.	Podatak o dužini stranice B tipa float dostupan samo unutar razreda
+        float stranicaB;
+
+        //4.	Metodu za dohvat vrijednosti varijable stranicaA
+        public float VratiStranicuA()
+        {
+            return stranicaA;
+        }
+        //5.	Metodu za postavljanje vrijednosti varijable stranicaA 
+        public void PostaviStranicuA(float strA)
+        {
+            // ako se definira negativna količina izbaciti grešku
+            if (strA < 0)
+                throw new Exception("Vrijednost mora biti veća od 0");
+
+            stranicaA = strA;
+        }
+
+        //6.	Metodu za dohvat vrijednosti varijable stranicaB
+        public float VratiStranicuB()
+        {
+            return stranicaB;
+        }
+
+        //6.	7.	Metodu za postavljanje vrijednosti varijable stranicaB 
+        public void PostaviStranicuB(float strB)
+        {
+            // ako se definira negativna količina izbaciti grešku
+            if (strB < 0)
+                throw new Exception("Vrijednost mora biti veća od 0");
+
+            stranicaB = strB;
+        }
+
+        //8.	Metodu koje vraća površinu pravokutnika
+        public float VratiPovrsinu()
+        {
+            return stranicaA * stranicaB;
+        }
+
+        //9.	Metodu koje vraća opseg pravokutnika
+        public float VratiOpseg()
+        {
+            return 2 * stranicaA + 2 * stranicaB;
+        }
+
+        //10.	Metodu za izračun dijagonale
+        public double VratiDijagonalu()
+        {
+            return Math.Sqrt(Math.Pow(stranicaA, 2) + Math.Pow(stranicaB, 2));
+        }
+
+        //11.	Metodu za provjeru ako je pravokutnik kvadrat
+        public bool JeKvadrat()
+        {
+            //PRVI NAČIN
+            /*if (stranicaA == stranicaB)
+                return true;
+            else
+                return false;*/
+
+            //ILI DRUGI NAČIN
+            //return stranicaA == stranicaB ? true : false;
+
+            //ILI TREĆI NAČIN
+            return stranicaA == stranicaB;
+        }
+
+    }
+}
---
+++ class/Tocka.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Vj3
+{
+    //1.	Ispravno definiran naziv razreda
+    class Tocka
+    {
+        //2.	Podatak o apscisi (x) tipa float dostupan samo unutar razreda
+        float x;
+        //3.	Podatak o ordinati (y) tipa float dostupan samo unutar razreda
+        float y;
+
+        //4.	Metodu za postavljanje vrijednosti varijable x
+        public void PostaviX(float x)
+        {
+            this.x = x;
+        }
+
+        //5.	Metodu za postavljanje vrijednosti varijable y
+        public void PostaviY(float y)
+        {
+            this.y = y;
+        }
+
+        //6.	Metodu za dohvat vrijednosti varijable x
+        public float VratiX()
+        {
+            return x;
+        }
+
+        //7.	Metodu za dohvat vrijednosti varijable y
+        public float VratiY()
+        {
+            return y;
+        }
+
+        //8.	Metodu za postavljanje obje varijable x i y u jednom pozivu
+        public void PostaviXY(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        //9.	Metodu za apsolutni pomak točke na x i y koja prima jedan parametar tipa Tocka
+        public void PomakniNa(Tocka t)
+        {
+            //kao parametar dobivamo objekt tipa Tocka te preslikamo njegove vrijednosti u varijable x i y
+            this.x = t.VratiX();
+            this.y = t.VratiY();
+        }
+
+        //10.	Metodu za relativni pomak točke u x i y smjeru (s obzirom na trenutni položaj)
+        public void PomakniZa(float dx, float dy)
+        {
+            x = x + dx;
+            y += dy;
+        }
+
+        //11.	Privatnu metodu za izračun udaljenosti do točke definirane pomoću dvije float koordinate
+        //formula je uvijek ista pa je pozivamo iz funkcija UdaljenostDo(Tocka t) i UdaljenostDo(float x, float y)
+        //čime izbjegavamo pisati istu formulu na 2 mjesta
+        private float Udaljenost(float x, float y)
+        {
+            //Math.Sqrt nam vraća rezultat u obliku double tipa podatka koji ima veću preciznost od
+            //tipa float (tj. može imati više decimala) kojeg vraća naša funkcija pa je potrebno
+            //pretvoriti double tip podatke u float na donji način: (float)Math... -> postupak koji zovemo casting
+            return (float)Math.Sqrt(Math.Pow(this.x - x, 2) + Math.Pow(this.y - y, 2));
+        }
+
+        //13.	Metodu za izračun udaljenosti do točke koja se metodi predaje kao tip Tocka
+        public float UdaljenostDo(Tocka t)
+        {
+            //pozivamo metodu Udaljenost kojoj prosljeđujemo vrijednosti varijabli x i y objekta t
+            return Udaljenost(t.VratiX(), t.VratiY());
+        }
+
+        //12.	Metodu za izračun udaljenosti do točke čiji se parametri predaju kao dvije koordinate tipa float
+        public float UdaljenostDo(float x, float y)
+        {
+            //pozivamo metodu Udaljenost kojoj prosljeđujemo vrijednosti varijabli x i y
+            return Udaljenost(x, y);
+        }
+
+    }
+}
