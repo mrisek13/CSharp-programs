688fcb68abc2e1cdb2aec848492a946582a673cb
Maja Risek <108302992+mrisek13@users.noreply.github.com>
2023-11-23 13:44:24 +0100

    Update Program.cs

--- 5. Constructor/Program.cs
+++ 5. Constructor/Program.cs
@@ -62,7 +62,84 @@ namespace OOP_Vj6
             Console.WriteLine("\nIspis košarice nakon ažuriranja stavaka...\n");
             IspisKosarice(k1);
 
-            Console.ReadKey();
+            Console.WriteLine("Dodatni zadaci:");
+
+            /*
+             * Dodatni zadaci:
+                Kreirati barem 2, 3 košarice, kupca, više stavaka i isprobati dodavanje/brisanje stavaka, pozivanje
+                metoda/svojstva za postavljanje vrijednosti kreiranih objekata te ispis košarica
+             */
+
+            VlasnikKosarice vlasnik1 = new VlasnikKosarice("456789", "Ivo Ivić", "Trg kralja Tomislava 10, OS");
+            vlasnik1.SetAdresa("Trg bana Josipa Jelačića 75, Đakovo");
+            vlasnik1.SetNaziv("Josip Josipović");
+
+            Kosarica kos1 = new Kosarica(vlasnik1);
+            kos1.DodajStavku("Mobitel Alcatel", 3, 399.99m);
+            kos1.DodajStavku("Mobitel Xiaomi", 2, 299.99m);
+            kos1.DodajStavku("Mobitel Samsung A50", 3, 499.99m);
+
+            StavkaKosarice stavka1 = new StavkaKosarice("Mobitel iPhone 12 Pro Max", 2, 1499.99m);
+            kos1.DodajStavku(stavka1);
+
+            stavka1.Cijena = 1299.99m;
+            stavka1.Kolicina = 3;
+            stavka1.Opis = "Mobitel iPhone 12";
+
+            kos1.BrisiStavku(0);
+            kos1.BrisiStavku(stavka1);
+
+            VlasnikKosarice vlasnik2 = new VlasnikKosarice("123456", "Maja Risek", "Zagrebačka 5, VŽ");
+            Kosarica kos2 = new Kosarica(vlasnik2);
+
+            kos2.DodajStavku("Laptop Dell D150", 2, 599.99m);
+            kos2.DodajStavku("Laptop ACER A200", 3, 699.99m);
+            kos2.DodajStavku("Laptop HP H340", 5, 999.99m);
+            kos2.DodajStavku("Laptop Toshiba T5", 1, 6399.99m);
+            kos2.DodajStavku("Laptop ACER A500", 3, 799.99m);
+            kos2.DodajStavku("Laptop HP H680", 5, 899.99m);
+
+            vlasnik2.SetNaziv("Ivan Horvat");
+            vlasnik2.SetAdresa("Zagrebačka 60, Varaždin");
+
+            StavkaKosarice stavka2 = new StavkaKosarice("Mobitel Motorola Edge 40", 2, 499.99m);
+            kos2.DodajStavku(stavka2);
+
+            stavka1.Cijena = 999.99m;
+            stavka1.Kolicina = 6;
+            stavka1.Opis = "Laptop Lenovo";
+
+            kos2.BrisiStavku(0);
+            kos2.BrisiStavku(stavka1);
+
+            VlasnikKosarice vlasnik3 = new VlasnikKosarice("789456", "Marko Marić", "Kantrida 25, RI");
+            vlasnik3.SetAdresa("Istarska 15, Rijeka");
+            vlasnik3.SetNaziv("Fran Franjić");
+            Kosarica kos3 = new Kosarica(vlasnik3);
+
+            kos3.DodajStavku("Mobitel Alcatel A20", 3, 199.99m);
+            kos3.DodajStavku("Mobitel Xiaomi X45", 2, 299.99m);
+            kos3.DodajStavku("Mobitel Samsung A50", 3, 399.99m);
+            kos3.DodajStavku("Mobitel Alcatel A80", 3, 399.99m);
+            kos3.DodajStavku("Mobitel Xiaomi X50", 2, 599.99m);
+            kos3.DodajStavku("Mobitel Samsung A30", 3, 499.99m);
+
+            StavkaKosarice stavka3 = new StavkaKosarice("Tablet Samsung", 3, 399.99m);
+            kos3.DodajStavku(stavka3);
+
+            stavka3.Cijena = 499.99m;
+            stavka3.Kolicina = 5;
+            stavka3.Opis = "Tablet Samsung";
+
+            kos3.BrisiStavku(0);
+            kos3.BrisiStavku(stavka1);
+
+            Console.WriteLine("\nIspis košarice nakon ažuriranja stavaka...\n");
+            IspisKosarice(kos1);
+            IspisKosarice(kos2);
+            IspisKosarice(kos3);
+
+            Console.ReadLine();
         }
 
         public static void IspisKosarice(Kosarica kos)
